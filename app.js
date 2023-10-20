cd78d483a122aa256b27b7edddb79935517d0f77
McMartinezc <psyence_mc@hotmail.com>
2023-10-20 13:29:59 +0200

    ok

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // ¿En tu páis de origen es normal dejar propina?
+const btn = document.getElementById("calculate");
+
+
+btn.addEventListener("click", () => {
+
+  const billAmount = parseInt(document.getElementById("bill-amount").value);
+  const percentageTip =  document.getElementById("percentage-tip").value;
+
+  let tip = billAmount * (+percentageTip / 100);
+  let totalPrice = billAmount + tip;
+
+  document.getElementById("tip-amount").value = tip;
+  document.getElementById("total").value = totalPrice;
+});
